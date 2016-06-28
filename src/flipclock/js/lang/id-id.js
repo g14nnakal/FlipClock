7ea879ecf30c58118bd4c070cb2da1a7135cd5e5
g14nnakal <g14nblog@gmail.com>
2016-06-28 18:43:57 +0700

    Created Indonesia i18n file.

---
+++ src/flipclock/js/lang/id-id.js
@@ -0,0 +1,28 @@
+(function($) {
+	
+	/**
+	 * FlipClock Indonesia Language Pack
+	 *
+	 * This class will used to translate tokens into the Indonesian language.
+	 *
+	 */
+	
+	FlipClock.Lang.Indonesian = {
+		
+		'years'   : 'tahun',
+		'months'  : 'bulan',
+		'days'    : 'hari',
+		'hours'   : 'jam',
+		'minutes' : 'menit',
+		'seconds' : 'detik'
+		
+	};
+	
+	/* Create various aliases for convenience */
+	
+	FlipClock.Lang['id']       = FlipClock.Lang.Indonesian;
+	FlipClock.Lang['id-id']    = FlipClock.Lang.Indonesian;
+	FlipClock.Lang['indonesian'] = FlipClock.Lang.Indonesian;
+	FlipClock.Lang['indonesia'] = FlipClock.Lang.Indonesian;
+	
+}(jQuery));
